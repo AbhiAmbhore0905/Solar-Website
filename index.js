e170fae55acd5304d2cc7505b1350fd995dd434d
Your Name <you@example.com>
2025-10-18 12:22:17 +0530

    updated backend with frontend integration

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
+const path = require('path')
 require("dotenv").config()
 
 const app = express()
 
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://solar-website-r78v.onrender.com",
     credentials: true
 }))
 
@@ -17,7 +20,8 @@ app.use("/api/auth", require("./routes/auth.route"))
 app.use("/api/message", require("./routes/message.route"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "resource not found" })
+    // res.status(404).json({ message: "resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 app.use((err, req, res, next) => {
     res.status(500).json({ message: "server error", error: err.message })
